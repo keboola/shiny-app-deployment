3bbd896635cfa3b98351d9afd9badb64a01f799a
pivnicek <marc@keboola.com>
2015-11-16 16:12:51 +0100

    trying different library

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ library('devtools')
 write("Installing shinyapps package",stderr())
 
 # install other necessary stuff from github
-devtools::install_github("rstudio/shinyapps", ref = "master")
+devtools::install_github("rstudio/rsconnect", ref = "master")
 
 # get the command line arguments
 args <- commandArgs(trailingOnly = TRUE)
@@ -17,7 +17,7 @@ secret <- args[2]
 
 write("Got Token and Secret",stderr())
 
-library(shinyapps)
+library(rsconnect)
 
 # now we need to hook up with our shinyappsio account
 #shinyapps::setAccountInfo(name='keboola', token=token, secret=secret)
