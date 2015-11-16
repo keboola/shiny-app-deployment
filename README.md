26af924068686d6a92b61414abfe1f912e5b0f90
pivnicek <marc@keboola.com>
2015-11-16 11:36:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Keboola Docker Shiny App
+Deploying Keboola Shiny applications.
+
+More details coming soon about building keboola shiny applications.
+
---
+++ main.R
@@ -0,0 +1,21 @@
+# we need devtools to get repos from github
+library('devtools')
+
+# install other necessary stuff from github
+devtools::install_github("rstudio/shinyapps", ref = "master")
+
+# get the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# first argument should be token
+token <- args[1]
+
+# second should be the secret
+secret <- args[2]
+
+# now we need to hook up with our shinyappsio account
+shinyapps::setAccountInfo(name='keboola', token=token, secret=secret)
+
+# cross fingers, and deploy (We are assuming that we're in the app home dir
+shinyapps::deployApp()
+
