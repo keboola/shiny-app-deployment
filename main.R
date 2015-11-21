b90e2d44abbc901410d1b9fb723a2b60fc96ccaa
pivnicek <marc@keboola.com>
2015-11-22 00:43:03 +0100

    feat: options rather than arguments + packages support

--- main.R
+++ main.R
@@ -1,3 +1,10 @@
+options(echo = TRUE)
+
+# set cran mirror location
+r <- getOption("repos")
+r["CRAN"] <- "http://cran.rstudio.com"
+options(repos = r)
+
 # we need devtools to get repos from github
 library('devtools')
 
@@ -15,51 +22,84 @@ write("Installing shinyapps package",stderr())
 # install other necessary stuff from github
 devtools::install_github("rstudio/shinyapps", ref = "master")
 
-# get the command line arguments
-args <- commandArgs(trailingOnly = TRUE)
-
-# What should this thing do?
-command <- args[1]
-
-# valid commands: deploy, archive [delete will be available in a future version]
-validCommands <- c("deploy", "archive")
-
-# make sure the given command is valid
-if (!(command %in% validCommands)) {
-    stop(paste("I'm sorry, but I don't know how to", command,". I only know how to", paste(validCommands,sep="and")))
-}
-
-# first argument should be token
-token <- args[2]
-
-# second should be the secret
-secret <- args[3]
-
-# Name of the application
-appName <- args[4]
-
-
-write("Got Token and Secret", stderr())
-
-# set cran mirror location
-r <- getOption("repos")
-r["CRAN"] <- "http://cran.rstudio.com"
-options(repos = r)
-
 library(shinyapps)
+library(optparse)
 
-# now we need to hook up with our shinyappsio account
-shinyapps::setAccountInfo(name='keboola', token=token, secret=secret)
+option_list <- list(
+    make_option(c("-v", "--verbose"), action="store_true", default=TRUE,
+                    help="Print extra output [default]"),
+    make_option(c("-d", "--command"), default="deploy",help="Either 'deploy' or 'archive'"),
+    
+    make_option(c("-a", "--account"), default="keboola",
+                    help="The account name for shinyApps.io [default %default]"),
+    make_option(c("-n", "--appName"), help = "The name of the application to be deployed"),
+    make_option(c("-t", "--token"), help = "The token to use for app deployment"),
+    make_option(c("-s", "--secret"), help = "The secret associated with the token"),
+    make_option(c("-r", "--repository"), help = "The repository url of the application"),
+	make_option(c("-u", "--username"), help = "The username for the repository if private"),
+	make_option(c("-p", "--password"), help = "The user's password for the repository if private"),
+	make_option(c("-c", "--cranPackages"), help = "list of cran packages to install"),
+	make_option(c("-g", "--githubPackages"), help = "list of github packages to install")
+)
 
-write("Account Info Set",stderr())
+opt <- parse_args(OptionParser(option_list=option_list))
 
-write(paste("attempting to deploy app", appName), stderr())
+print("received options")
 
-if (command == "deploy") {
-    # cross fingers, and deploy (We are assuming that we're in the app home dir
-    deployApp(appDir="/home/app", appName=appName)
-} else if (command == "archive") {
-    terminateApp(appName=appName)
+print(opt)
+
+
+allArgs <- c("help","verbose","command","account","appName",
+			 "token","secret","repository","username",
+			 "password","cranPackages","githubPackages")
+if (opt$command == "deploy") {
+	requiredArgs <- c("appName","token","secret","repository")
+} else if (opt$command == "archive") {
+	requiredArgs <- c("appName","token","secret","repository")
 } else {
-    stop(paste("You shouldn't be here. I thought I said that I didn't know how to", command))
+	stop("sorry, the option -c or --command must be either 'deploy' or 'archive'")
 }
+
+missingArgs <- requiredArgs[!(requiredArgs %in% names(opt))]
+unknownArgs <- names(opt)[!(names(opt) %in% allArgs)]
+
+if (length(unknownArgs) > 0) {
+	stop(paste("Sorry, I don't understand the following options:",paste(unknownArgs,collapse=", ")))
+}
+
+if (length(missingArgs) > 0) {
+	stop(paste("Sorry, the following required options are missing:",paste(missingArgs,collapse=", ")))
+}
+	
+# call into shinyapps with keboola account credentials
+shinyapps::setAccountInfo(name='keboola', token=opt$token, secret=opt$secret)
+
+if (opt$command == "archive") {
+	# archive the application
+	terminateApp(appName=opt$appName)
+} else {
+	# define a helper function for trimming strings (is there no base r function for this?)
+	trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+	if (!is.null(opt$cranPackages)) {
+		# we need to install any packages that we've been told to
+		cranPackages <- trim(unlist(strsplit(opt$cranPackages,",")))
+		print(cranPackages)
+		lapply(cranPackages, function(x){
+			print(paste("Installing package",x,"from cran."))
+			install.packages(x, verbose=FALSE, quiet=TRUE)
+		})
+	} else print("No cran packages to install")
+	if (!is.null(opt$githubPackages)){
+		# we need to install any github packages that we've been told to.
+		githubPackages <- trim(unlist(strsplit(opt$githubPackages,",")))
+		print(githubPackages)
+		lapply(githubPackages, function(x){
+			print(paste("Installing package",x,"from github."))
+			devtools::install_github(x)
+		})
+	} else print("No github packages to install")
+	# and deploy the app
+	deployApp(appDir="/home/app", appName=opt$appName)	
+}
+
+# end
